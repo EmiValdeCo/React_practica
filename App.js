124aa5388d5b726e358568a6aec633d1dd2e33ef
EmiValdeCo <20190010@ricaldone.edu.sv>
2024-05-02 23:18:39 -0600

    funcion del menu completo y el todolist

--- App.js
+++ App.js
@@ -1,111 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
-import Inputs from './src/components/inputs';
-import Boton from './src/components/boton';
-import { useState } from 'react'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Calculadora from './src/screen/Calculadore';
+import TodoList from './src/screen/todoList';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const [numero1, setNumero1] = useState(0);
-  const [numero2, setNumero2] = useState(0);
-  const [resultado, setResultado] = useState(0);
-
-  const sumar = () => {
-    setResultado(parseFloat(numero1) + parseFloat(numero2));
-  }
-
-  const restar = () => {
-    setResultado(parseFloat(numero1) - parseFloat(numero2));
-  }
-
-  const multiplicar = () => {
-    setResultado(parseFloat(numero1) * parseFloat(numero2));
-  }
-
-  const dividir = () => {
-    if (numero2 !== 0) {
-      setResultado(parseFloat(numero1) / parseFloat(numero2));
-    } else {
-      alert('No se puede dividir por cero');
-    }
-  }
-
-  const reset = () => {
-    setResultado(0);
-    setNumero1(0)
-    setNumero2(0)
-  }
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.texto}>Calculadora de números</Text>
-      <Inputs
-        titulo_input={'Numero 1'}
-        tipo_keyboard={'number-pad'}
-        numero={numero1}
-        setNumero={setNumero1}
-      />
-      <Inputs
-        titulo_input={'Numero 2'}
-        tipo_keyboard={'number-pad'}
-        numero={numero2}
-        setNumero={setNumero2}
-      />
-      <View style={styles.botonesContainer}>
-        <View style={styles.botonesContainer1}>
-          <Boton
-            textBoton={'Suma'}
-            evento={sumar}
-          />
-          <Boton
-            textBoton={'Restar'}
-            evento={restar}
-          />
-        </View>
-        <View style={styles.botonesContainer2}>
-          <Boton
-            textBoton={'Multiplicar'}
-            evento={multiplicar}
-          />
-          <Boton
-            textBoton={'Dividir'}
-            evento={dividir}
-          />
-        </View>
-        <Boton
-          textBoton={'Reset'}
-          evento={reset}
-        />
-      </View>
-      <Text>Resultado: {resultado}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Calculadora" component={Calculadora} />
+        <Tab.Screen name="TodoList" component={TodoList} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  texto: {
-    color: 'black',
-    fontSize: 30,
-    padding: 10,
-    marginBottom: 20
-  },
-  botonesContainer: {
-    flexDirection: 'column',
-    marginTop: 10,
-    marginBottom: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  botonesContainer1: {
-    flexDirection: 'row'
-  },
-  botonesContainer2: {
-    flexDirection: 'row'
-  }
-});
